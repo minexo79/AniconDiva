9ef1b9aaa9a65d3bd4acea1677a252622ee0a88c
minexo79 <minexo79@gmail.com>
2025-08-01 22:53:29 +0800

    Fix Debug Mode In Env

--- source/utils/envload.py
+++ source/utils/envload.py
@@ -9,7 +9,7 @@ def load_environment_variables(app):
     app.config['SECRET_KEY']           = os.environ.get("WEB_SECRET_KEY", "Your_Super_Secret_Key_123456")
     app.config['ADMIN_PSWD']           = os.environ.get("ADMIN_PASSWORD", "123456789abcdef")
     app.config['HASH_SALT']            = os.environ.get("PASSWORD_HASH_SALT", "MyUltraHashSalt_XYZ")
-    app.config['DEBUG']                = os.environ.get("DEBUG_MODE", True)
+    app.config['DEBUG']                = os.environ.get("DEBUG_MODE", 'True')
     app.config['DISCORD_POSTED_URL']   = os.environ.get("DISCORD_POSTED_WEBHOOK", None)
     app.config['DISCORD_VERIFY_URL']   = os.environ.get("DISCORD_VERIFIED_WEBHOOK", None)
     app.config['MYSQL_URL']            = os.environ.get("MYSQL_URL", None)
